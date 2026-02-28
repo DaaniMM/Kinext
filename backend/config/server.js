b986cb7b123237d44b361ba544f6141e4e1db2d8
Ubuntu <ubuntu@ip-172-31-20-167.eu-north-1.compute.internal>
2026-02-28 13:27:34 +0000

    config: strapi server.js con PUBLIC_URL

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://kinext.es'),
+  admin: {
+    url: '/admin',
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
